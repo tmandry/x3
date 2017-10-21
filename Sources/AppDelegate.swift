f3110b213b710ad73262b6f35fd26684c38911e2
Tyler Mandry <tmandry@gmail.com>
2017-10-21 18:35:41 -0500

    Create shell project

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  x3
+//
+//  Created by Tyler Mandry on 10/21/17.
+//  Copyright © 2017 Tyler Mandry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
