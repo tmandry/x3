b67ae7ca07d029380533d4ca363779c4d4ce04e7
Tyler Mandry <tmandry@gmail.com>
2024-01-04 23:57:29 -0800

    Application and event handler threads

--- crates/swell/src/main.rs
+++ crates/swell/src/main.rs
@@ -1,4 +1,9 @@
-use std::{future::Future, time::Instant};
+use std::{
+    future::Future,
+    sync::{self, mpsc::Sender},
+    thread,
+    time::Instant,
+};
 
 use accessibility::{AXUIElement, AXUIElementAttributes};
 use core_foundation::{
@@ -13,12 +18,10 @@ use core_graphics_types::geometry::CGRect;
 use icrate::{
     objc2::{
         declare_class, msg_send_id, mutability, rc::Allocated, rc::Id, sel, ClassType,
-        DeclaredClass,
+        DeclaredClass, Encode, Encoding,
     },
     AppKit::{self, NSApplication, NSWorkspace},
-    Foundation::{
-        MainThreadMarker, NSNotification, NSNotificationCenter, NSNotificationName, NSObject,
-    },
+    Foundation::{MainThreadMarker, NSNotification, NSNotificationCenter, NSObject},
 };
 use structopt::StructOpt;
 use tokio::sync::mpsc;
@@ -43,7 +46,9 @@ async fn main() {
         get_windows_with_ax(&opt, false, false)
     })
     .await;
-    watch_for_notifications()
+    let (init, events) = spawn_event_handler(&opt);
+    spawn_app_threads(&opt, init, events.clone());
+    watch_for_notifications(events)
 }
 
 #[allow(dead_code)]
@@ -69,21 +74,31 @@ async fn get_windows_with_cg(_opt: &Opt, print: bool) {
     println!("main display = {screen:?}");
 }
 
+fn running_apps(opt: &Opt) -> impl Iterator<Item = String> {
+    let bundle = opt.bundle.clone();
+    unsafe { NSWorkspace::sharedWorkspace().runningApplications() }
+        .into_iter()
+        .flat_map(move |app| {
+            let bundle_id = unsafe { app.bundleIdentifier() }?.to_string();
+            if let Some(filter) = &bundle {
+                if !bundle_id.contains(filter) {
+                    return None;
+                }
+            }
+            Some(bundle_id)
+        })
+}
+
 async fn get_windows_with_ax(opt: &Opt, serial: bool, print: bool) {
     let (sender, mut receiver) = mpsc::unbounded_channel();
-    for app in unsafe { NSWorkspace::sharedWorkspace().runningApplications() } {
-        let bundle_id = unsafe { app.bundleIdentifier() };
-        let Some(bundle_id) = bundle_id else { continue };
-        let bundle_str = bundle_id.to_string();
-        if let Some(filter) = &opt.bundle {
-            if &bundle_str != filter {
-                continue;
-            }
-        }
+    for bundle_id in running_apps(opt) {
         let sender = sender.clone();
         let task = move || {
-            let windows = get_windows_for_app(&bundle_str);
-            sender.send((bundle_str, windows)).unwrap()
+            let Ok(app) = AXUIElement::application_with_bundle(&bundle_id) else {
+                return;
+            };
+            let windows = get_windows_for_app(app);
+            sender.send((bundle_id, windows)).unwrap()
         };
         if serial {
             task();
@@ -107,9 +122,8 @@ async fn get_windows_with_ax(opt: &Opt, serial: bool, print: bool) {
     }
 }
 
-fn get_windows_for_app(bundle_id: &str) -> Result<Vec<Window>, accessibility::Error> {
+fn get_windows_for_app(app: AXUIElement) -> Result<Vec<Window>, accessibility::Error> {
     // TODO: Can't access processIdentifier for some reason.
-    let app = AXUIElement::application_with_bundle(&bundle_id).unwrap();
     let Ok(windows) = &app.windows() else {
         return Err(accessibility::Error::NotFound);
     };
@@ -133,7 +147,63 @@ async fn time<O, F: Future<Output = O>>(desc: &str, f: impl FnOnce() -> F) -> O 
     out
 }
 
-fn watch_for_notifications() {
+#[derive(Debug)]
+enum Event {
+    WindowMoved,
+    ApplicationActivated,
+    ApplicationLaunched(i32),
+    ApplicationTerminated(i32),
+    ScreenParametersChanged,
+}
+
+fn spawn_event_handler(_opt: &Opt) -> (Sender<Vec<Window>>, Sender<Event>) {
+    let (initial_windows_tx, initial_windows) = sync::mpsc::channel();
+    let (events_tx, events) = sync::mpsc::channel::<Event>();
+    thread::spawn(move || {
+        println!("\nInitial windows:");
+        for windows in initial_windows {
+            for window in windows {
+                println!("- {window:?}");
+            }
+        }
+        println!();
+
+        for event in events {
+            println!("Event {event:?}")
+        }
+    });
+    (initial_windows_tx, events_tx)
+}
+
+fn spawn_app_threads(
+    opt: &Opt,
+    initial_windows_tx: Sender<Vec<Window>>,
+    _events_tx: Sender<Event>,
+) {
+    for bundle_id in running_apps(opt) {
+        let tx = initial_windows_tx.clone();
+        let _ = thread::spawn(move || {
+            let Ok(app) = AXUIElement::application_with_bundle(&bundle_id) else {
+                return;
+            };
+            let Ok(windows) = get_windows_for_app(app) else {
+                return;
+            };
+            tx.send(windows).unwrap();
+        });
+    }
+}
+
+fn watch_for_notifications(events_tx: Sender<Event>) {
+    #[repr(C)]
+    struct Instance {
+        events_tx: &'static mut Sender<Event>,
+    }
+
+    unsafe impl Encode for Instance {
+        const ENCODING: Encoding = Encoding::Object;
+    }
+
     declare_class! {
         struct NotificationHandler;
 
@@ -147,70 +217,97 @@ fn watch_for_notifications() {
             const NAME: &'static str = "NotificationHandler";
         }
 
-        impl DeclaredClass for NotificationHandler {}
+        impl DeclaredClass for NotificationHandler {
+            type Ivars = Box<Instance>;
+        }
 
+        // SAFETY: Each of these method signatures must match their invocations.
         unsafe impl NotificationHandler {
-            #[method_id(init)]
-            fn init(this: Allocated<Self>) -> Option<Id<Self>> {
-                let this = this.set_ivars(());
+            #[method_id(initWith:)]
+            fn init(this: Allocated<Self>, instance: Instance) -> Option<Id<Self>> {
+                let this = this.set_ivars(Box::new(instance));
                 unsafe { msg_send_id![super(this), init] }
             }
 
-            #[method(handle:)]
-            fn handle(&self, notification: &NSNotification) {
-                println!("Got notification: {notification:?}")
+            #[method(handleActivated:)]
+            fn handle_activated(&self, _notif: &NSNotification) {
+                self.send_event(Event::ApplicationActivated);
+            }
+
+            #[method(handleLaunched:)]
+            fn handle_launched(&self, _notif: &NSNotification) {
+                // TODO: pid
+                self.send_event(Event::ApplicationLaunched(0));
+            }
+
+            #[method(handleTerminated:)]
+            fn handle_terminated(&self, _notif: &NSNotification) {
+                // TODO: pid
+                self.send_event(Event::ApplicationTerminated(0));
+            }
+
+            #[method(handleScreenChanged:)]
+            fn handle_screen_changed(&self, _notif: &NSNotification) {
+                self.send_event(Event::ScreenParametersChanged);
             }
         }
     }
 
     impl NotificationHandler {
-        fn new() -> Id<Self> {
-            unsafe { msg_send_id![Self::alloc(), init] }
+        fn new(events_tx: Sender<Event>) -> Id<Self> {
+            let events_tx = Box::leak(Box::new(events_tx));
+            let instance = Instance { events_tx };
+            unsafe { msg_send_id![Self::alloc(), initWith: instance] }
+        }
+
+        fn send_event(&self, event: Event) {
+            if let Err(err) = self.ivars().events_tx.send(event) {
+                eprintln!("Warning: Failed to send event: {err:?}");
+            }
         }
     }
 
-    let handler = NotificationHandler::new();
+    let handler = NotificationHandler::new(events_tx);
 
-    let workspace = unsafe { NSWorkspace::sharedWorkspace() };
-    let workspace_center = unsafe { workspace.notificationCenter() };
+    // SAFETY: Selector must have signature fn(&self, &NSNotification)
+    let register_unsafe = |selector, notif_name, center: &Id<NSNotificationCenter>, object| unsafe {
+        center.addObserver_selector_name_object(&handler, selector, Some(notif_name), Some(object));
+    };
 
-    let workspace_notifs = unsafe {
+    let workspace = &unsafe { NSWorkspace::sharedWorkspace() };
+    let workspace_center = &unsafe { workspace.notificationCenter() };
+    let default_center = &unsafe { NSNotificationCenter::defaultCenter() };
+    let shared_app = &NSApplication::sharedApplication(MainThreadMarker::new().unwrap());
+    unsafe {
         use AppKit::*;
-        [
+        register_unsafe(
+            sel!(handleActivated:),
             NSWorkspaceDidActivateApplicationNotification,
-            // NSWorkspaceDidDeactivateApplicationNotification,
+            workspace_center,
+            workspace,
+        );
+        register_unsafe(
+            sel!(handleLaunched:),
             NSWorkspaceDidLaunchApplicationNotification,
+            workspace_center,
+            workspace,
+        );
+        register_unsafe(
+            sel!(handleTerminated:),
             NSWorkspaceDidTerminateApplicationNotification,
-        ]
+            workspace_center,
+            workspace,
+        );
+        register_unsafe(
+            sel!(handleScreenChanged:),
+            NSApplicationDidChangeScreenParametersNotification,
+            default_center,
+            shared_app,
+        );
     };
-    for notif_name in workspace_notifs {
-        unsafe {
-            workspace_center.addObserver_selector_name_object(
-                &handler,
-                sel!(handle:),
-                Some(notif_name),
-                Some(&workspace),
-            );
-        }
-    }
 
-    let default_center = unsafe { NSNotificationCenter::defaultCenter() };
-    let shared_app = NSApplication::sharedApplication(MainThreadMarker::new().unwrap());
-
-    let shared_app_notifs = unsafe {
-        use AppKit::*;
-        [NSApplicationDidChangeScreenParametersNotification]
-    };
-    for notif_name in shared_app_notifs {
-        unsafe {
-            default_center.addObserver_selector_name_object(
-                &handler,
-                sel!(handle:),
-                Some(notif_name),
-                Some(&shared_app),
-            );
-            CFRunLoopRun();
-        }
+    unsafe {
+        CFRunLoopRun();
     }
 }
 
