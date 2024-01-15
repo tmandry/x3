9ec306980bf967cd3b8d3ffd27ca85380878d29c
Tyler Mandry <tmandry@gmail.com>
2024-01-14 17:29:59 -0800

    More spaces APIs

--- crates/swell/examples/devtool.rs
+++ crates/swell/examples/devtool.rs
@@ -2,7 +2,13 @@
 
 use swell::space;
 
+use icrate::{AppKit::NSScreen, Foundation::MainThreadMarker};
+
 fn main() {
-    let space = space::cur_space();
-    println!("Current space: {space:?}");
+    println!("Current space: {:?}", space::cur_space());
+    println!("Visible spaces: {:?}", space::visible_spaces());
+    println!("All spaces: {:?}", space::all_spaces());
+    let screens = NSScreen::screens(MainThreadMarker::new().unwrap());
+    let screens: Vec<_> = screens.iter().map(|screen| screen.visibleFrame()).collect();
+    println!("Screen sizes: {screens:?}");
 }
--- crates/swell/src/space.rs
+++ crates/swell/src/space.rs
@@ -1,5 +1,11 @@
 use std::ffi::c_int;
 
+use bitflags::bitflags;
+use core_foundation::{
+    array::{CFArray, CFArrayRef},
+    base::TCFType,
+};
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct SpaceId(u64);
@@ -8,10 +14,53 @@ pub fn cur_space() -> SpaceId {
     SpaceId(unsafe { CGSGetActiveSpace(CGSMainConnectionID()) })
 }
 
+#[allow(dead_code)]
+pub fn visible_spaces() -> CFArray<SpaceId> {
+    unsafe {
+        let arr = CGSCopySpaces(CGSMainConnectionID(), CGSSpaceMask::ALL_VISIBLE_SPACES);
+        CFArray::wrap_under_create_rule(arr)
+    }
+}
+
+#[allow(dead_code)]
+pub fn all_spaces() -> CFArray<SpaceId> {
+    unsafe {
+        let arr = CGSCopySpaces(CGSMainConnectionID(), CGSSpaceMask::ALL_SPACES);
+        CFArray::wrap_under_create_rule(arr)
+    }
+}
+
 // Based on https://github.com/asmagill/hs._asm.undocumented.spaces/blob/master/CGSSpace.h.
 
 #[link(name = "CoreGraphics", kind = "framework")]
 extern "C" {
     fn CGSMainConnectionID() -> c_int;
     fn CGSGetActiveSpace(cid: c_int) -> u64;
+    fn CGSCopySpaces(cid: c_int, mask: CGSSpaceMask) -> CFArrayRef;
+}
+
+bitflags! {
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    #[repr(transparent)]
+    struct CGSSpaceMask: c_int {
+        const INCLUDE_CURRENT = 1 << 0;
+        const INCLUDE_OTHERS  = 1 << 1;
+
+        const INCLUDE_USER    = 1 << 2;
+        const INCLUDE_OS      = 1 << 3;
+
+        const VISIBLE         = 1 << 16;
+
+        const CURRENT_SPACES = Self::INCLUDE_USER.bits() | Self::INCLUDE_CURRENT.bits();
+        const OTHER_SPACES = Self::INCLUDE_USER.bits() | Self::INCLUDE_OTHERS.bits();
+        const ALL_SPACES =
+            Self::INCLUDE_USER.bits() | Self::INCLUDE_OTHERS.bits() | Self::INCLUDE_CURRENT.bits();
+
+        const ALL_VISIBLE_SPACES = Self::ALL_SPACES.bits() | Self::VISIBLE.bits();
+
+        const CURRENT_OS_SPACES = Self::INCLUDE_OS.bits() | Self::INCLUDE_CURRENT.bits();
+        const OTHER_OS_SPACES = Self::INCLUDE_OS.bits() | Self::INCLUDE_OTHERS.bits();
+        const ALL_OS_SPACES =
+            Self::INCLUDE_OS.bits() | Self::INCLUDE_OTHERS.bits() | Self::INCLUDE_CURRENT.bits();
+    }
 }
