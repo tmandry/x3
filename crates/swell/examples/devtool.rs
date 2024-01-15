f5daf3020f193e08d83e764b56b27baab27165bc
Tyler Mandry <tmandry@gmail.com>
2024-01-14 20:57:15 -0800

    Move timing code to devtool

--- crates/swell/examples/devtool.rs
+++ crates/swell/examples/devtool.rs
@@ -1,11 +1,40 @@
 //! This tool is used to exercise swell and system APIs during development.
 
-use core_graphics::display::CGMainDisplayID;
-use swell::space;
+use std::{future::Future, time::Instant};
 
+use accessibility::{AXUIElement, AXUIElementAttributes};
+use core_foundation::{array::CFArray, base::TCFType, dictionary::CFDictionaryRef};
+use core_graphics::{
+    display::{CGDisplayBounds, CGMainDisplayID},
+    window::{kCGNullWindowID, kCGWindowListOptionOnScreenOnly, CGWindowListCopyWindowInfo},
+};
 use icrate::{AppKit::NSScreen, Foundation::MainThreadMarker};
+use structopt::StructOpt;
+use tokio::sync::mpsc;
 
-fn main() {
+use swell::{app, reactor, space};
+
+#[derive(StructOpt)]
+pub struct Opt {
+    pub bundle: Option<String>,
+    pub resize: Option<String>,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let opt = Opt::from_args();
+    //time("accessibility serial", || get_windows_with_ax(&opt, true)).await;
+    time("core-graphics", || get_windows_with_cg(&opt, true)).await;
+    time("accessibility", || get_windows_with_ax(&opt, false, true)).await;
+    time("core-graphics second time", || {
+        get_windows_with_cg(&opt, false)
+    })
+    .await;
+    time("accessibility second time", || {
+        get_windows_with_ax(&opt, false, false)
+    })
+    .await;
+
     println!("Current space: {:?}", space::cur_space());
     println!("Visible spaces: {:?}", space::visible_spaces());
     println!("All spaces: {:?}", space::all_spaces());
@@ -19,4 +48,65 @@ fn main() {
     println!("Screen sizes: {frames:?}");
     let descrs: Vec<_> = screens.iter().map(|screen| screen.deviceDescription()).collect();
     println!("Screen descrs: {descrs:?}");
+}
+
+async fn get_windows_with_cg(_opt: &Opt, print: bool) {
+    let windows: CFArray<CFDictionaryRef> = unsafe {
+        CFArray::wrap_under_get_rule(CGWindowListCopyWindowInfo(
+            kCGWindowListOptionOnScreenOnly,
+            kCGNullWindowID,
+        ))
+    };
+    if print {
+        println!("{windows:?}");
+    }
+    let display_id = unsafe { CGMainDisplayID() };
+    let screen = unsafe { CGDisplayBounds(display_id) };
+    println!("main display = {screen:?}");
+}
+
+async fn get_windows_with_ax(opt: &Opt, serial: bool, print: bool) {
+    let (sender, mut receiver) = mpsc::unbounded_channel();
+    for (pid, bundle_id) in app::running_apps(opt.bundle.clone()) {
+        let sender = sender.clone();
+        let task = move || {
+            let app = AXUIElement::application(pid);
+            let windows = get_windows_for_app(app);
+            sender.send((bundle_id, windows)).unwrap()
+        };
+        if serial {
+            task();
+        } else {
+            tokio::task::spawn_blocking(task);
+        }
+    }
+    drop(sender);
+    while let Some((info, windows)) = receiver.recv().await {
+        //println!("{info:?}");
+        match windows {
+            Ok(windows) => {
+                if print {
+                    for win in windows {
+                        println!("{win:?} from {}", info.bundle_id.as_deref().unwrap_or("?"));
+                    }
+                }
+            }
+            Err(_) => (), //println!("  * Error reading windows: {err:?}"),
+        }
+    }
+}
+
+fn get_windows_for_app(app: AXUIElement) -> Result<Vec<reactor::Window>, accessibility::Error> {
+    let Ok(windows) = &app.windows() else {
+        return Err(accessibility::Error::NotFound);
+    };
+    windows.into_iter().map(|win| reactor::Window::try_from(&*win)).collect()
+}
+
+async fn time<O, F: Future<Output = O>>(desc: &str, f: impl FnOnce() -> F) -> O {
+    let start = Instant::now();
+    let out = f().await;
+    let end = Instant::now();
+    println!("{desc} took {:?}", end - start);
+    out
 }
--- crates/swell/src/main.rs
+++ crates/swell/src/main.rs
@@ -5,41 +5,11 @@ mod reactor;
 mod run_loop;
 mod space;
 
-use std::{future::Future, time::Instant};
-
-use accessibility::{AXUIElement, AXUIElementAttributes};
-use core_foundation::{array::CFArray, base::TCFType, dictionary::CFDictionaryRef};
-use core_graphics::{
-    display::{CGDisplayBounds, CGMainDisplayID},
-    window::{kCGNullWindowID, kCGWindowListOptionOnScreenOnly, CGWindowListCopyWindowInfo},
-};
 use hotkey::{HotkeyManager, KeyCode, Modifiers};
-use log::debug;
 use reactor::{Command, Event, Sender};
-use structopt::StructOpt;
-use tokio::sync::mpsc;
 
-#[derive(StructOpt)]
-pub struct Opt {
-    pub bundle: Option<String>,
-    pub resize: Option<String>,
-}
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
+fn main() {
     env_logger::init();
-    let opt = Opt::from_args();
-    //time("accessibility serial", || get_windows_with_ax(&opt, true)).await;
-    time("core-graphics", || get_windows_with_cg(&opt, true)).await;
-    time("accessibility", || get_windows_with_ax(&opt, false, true)).await;
-    time("core-graphics second time", || {
-        get_windows_with_cg(&opt, false)
-    })
-    .await;
-    time("accessibility second time", || {
-        get_windows_with_ax(&opt, false, false)
-    })
-    .await;
     let events = reactor::Reactor::spawn();
     app::spawn_initial_app_threads(events.clone());
     let _mgr = register_hotkeys(events.clone());
@@ -51,65 +21,4 @@ fn register_hotkeys(events: Sender<Event>) -> HotkeyManager {
     mgr.register(Modifiers::ALT, KeyCode::KeyW, Command::Hello);
     mgr.register(Modifiers::ALT, KeyCode::KeyS, Command::Shuffle);
     mgr
-}
-
-async fn get_windows_with_cg(_opt: &Opt, print: bool) {
-    let windows: CFArray<CFDictionaryRef> = unsafe {
-        CFArray::wrap_under_get_rule(CGWindowListCopyWindowInfo(
-            kCGWindowListOptionOnScreenOnly,
-            kCGNullWindowID,
-        ))
-    };
-    if print {
-        debug!("{windows:?}");
-    }
-    let display_id = unsafe { CGMainDisplayID() };
-    let screen = unsafe { CGDisplayBounds(display_id) };
-    debug!("main display = {screen:?}");
-}
-
-async fn get_windows_with_ax(opt: &Opt, serial: bool, print: bool) {
-    let (sender, mut receiver) = mpsc::unbounded_channel();
-    for (pid, bundle_id) in app::running_apps(opt.bundle.clone()) {
-        let sender = sender.clone();
-        let task = move || {
-            let app = AXUIElement::application(pid);
-            let windows = get_windows_for_app(app);
-            sender.send((bundle_id, windows)).unwrap()
-        };
-        if serial {
-            task();
-        } else {
-            tokio::task::spawn_blocking(task);
-        }
-    }
-    drop(sender);
-    while let Some((info, windows)) = receiver.recv().await {
-        //debug!("{info:?}");
-        match windows {
-            Ok(windows) => {
-                if print {
-                    for win in windows {
-                        debug!("{win:?} from {}", info.bundle_id.as_deref().unwrap_or("?"));
-                    }
-                }
-            }
-            Err(_) => (), //debug!("  * Error reading windows: {err:?}"),
-        }
-    }
-}
-
-fn get_windows_for_app(app: AXUIElement) -> Result<Vec<reactor::Window>, accessibility::Error> {
-    let Ok(windows) = &app.windows() else {
-        return Err(accessibility::Error::NotFound);
-    };
-    windows.into_iter().map(|win| reactor::Window::try_from(&*win)).collect()
-}
-
-async fn time<O, F: Future<Output = O>>(desc: &str, f: impl FnOnce() -> F) -> O {
-    let start = Instant::now();
-    let out = f().await;
-    let end = Instant::now();
-    debug!("{desc} took {:?}", end - start);
-    out
 }
