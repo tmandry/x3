c78605ca988afa5ea29e83629149692414608b72
Tyler Mandry <tmandry@gmail.com>
2024-01-14 19:32:38 -0800

    More spaces APIs

--- crates/swell/examples/devtool.rs
+++ crates/swell/examples/devtool.rs
@@ -1,5 +1,6 @@
 //! This tool is used to exercise swell and system APIs during development.
 
+use core_graphics::display::CGMainDisplayID;
 use swell::space;
 
 use icrate::{AppKit::NSScreen, Foundation::MainThreadMarker};
@@ -8,7 +9,14 @@ fn main() {
     println!("Current space: {:?}", space::cur_space());
     println!("Visible spaces: {:?}", space::visible_spaces());
     println!("All spaces: {:?}", space::all_spaces());
+    println!("{:?}", space::managed_display_spaces());
+    println!("CG screens: {:?}, main={}", space::screens(), unsafe {
+        CGMainDisplayID()
+    });
+    println!("{:?}", space::managed_displays());
     let screens = NSScreen::screens(MainThreadMarker::new().unwrap());
-    let screens: Vec<_> = screens.iter().map(|screen| screen.visibleFrame()).collect();
-    println!("Screen sizes: {screens:?}");
+    let frames: Vec<_> = screens.iter().map(|screen| screen.visibleFrame()).collect();
+    println!("Screen sizes: {frames:?}");
+    let descrs: Vec<_> = screens.iter().map(|screen| screen.deviceDescription()).collect();
+    println!("Screen descrs: {descrs:?}");
 }
--- crates/swell/src/space.rs
+++ crates/swell/src/space.rs
@@ -1,10 +1,12 @@
-use std::ffi::c_int;
+use std::{ffi::c_int, mem::MaybeUninit};
 
 use bitflags::bitflags;
 use core_foundation::{
     array::{CFArray, CFArrayRef},
     base::TCFType,
 };
+use core_graphics::display::CGGetActiveDisplayList;
+use core_graphics_types::base::{kCGErrorSuccess, CGError};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[repr(transparent)]
@@ -30,13 +32,44 @@ pub fn all_spaces() -> CFArray<SpaceId> {
     }
 }
 
+type CGDirectDisplayID = u32;
+
+pub fn screens() -> Result<Vec<CGDirectDisplayID>, CGError> {
+    const MAX_SCREENS: usize = 64;
+    let mut ids: MaybeUninit<[CGDirectDisplayID; MAX_SCREENS]> = MaybeUninit::uninit();
+    let mut count: u32 = 0;
+    let ids = unsafe {
+        let err = CGGetActiveDisplayList(
+            MAX_SCREENS as u32,
+            ids.as_mut_ptr() as *mut CGDirectDisplayID,
+            &mut count,
+        );
+        if err != kCGErrorSuccess {
+            return Err(err);
+        }
+        std::slice::from_raw_parts(ids.as_ptr() as *const u32, count as usize)
+    };
+    Ok(ids.iter().copied().collect())
+}
+
+pub fn managed_displays() -> CFArray {
+    unsafe { CFArray::wrap_under_create_rule(CGSCopyManagedDisplays(CGSMainConnectionID())) }
+}
+
+pub fn managed_display_spaces() -> CFArray<SpaceId> {
+    unsafe { CFArray::wrap_under_create_rule(CGSCopyManagedDisplaySpaces(CGSMainConnectionID())) }
+}
+
 // Based on https://github.com/asmagill/hs._asm.undocumented.spaces/blob/master/CGSSpace.h.
+// Also see https://github.com/koekeishiya/yabai/blob/d55a647913ab72d8d8b348bee2d3e59e52ce4a5d/src/misc/extern.h.
 
 #[link(name = "CoreGraphics", kind = "framework")]
 extern "C" {
     fn CGSMainConnectionID() -> c_int;
     fn CGSGetActiveSpace(cid: c_int) -> u64;
     fn CGSCopySpaces(cid: c_int, mask: CGSSpaceMask) -> CFArrayRef;
+    fn CGSCopyManagedDisplays(cid: c_int) -> CFArrayRef;
+    fn CGSCopyManagedDisplaySpaces(cid: c_int) -> CFArrayRef;
 }
 
 bitflags! {
