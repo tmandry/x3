34eb4c41fa031a13596f07cf017361fc24ccbd68
Tyler Mandry <tmandry@gmail.com>
2024-01-18 21:54:56 -0800

    Add BSP layout

--- crates/swell/src/reactor.rs
+++ crates/swell/src/reactor.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync, thread};
+use std::{collections::HashMap, iter, sync, thread};
 
 use icrate::Foundation::{CGPoint, CGRect, CGSize};
 use log::{debug, info};
@@ -161,9 +161,13 @@ impl Reactor {
             .iter()
             .map(|wid| (&self.apps[&wid.pid].info, &self.windows[&wid]))
             .collect();
-        debug!("Window list: {list:#?}");
+        info!("Window list: {list:?}");
         info!("Screen: {main_screen:?}");
-        let layout = calculate_layout(main_screen.frame.clone(), &list);
+        let layout = calculate_layout(
+            main_screen.frame.clone(),
+            &list,
+            Layout::Bsp(Orientation::Horizontal),
+        );
         info!("Layout: {layout:?}");
 
         let mut anim = Animation::new();
@@ -177,23 +181,99 @@ impl Reactor {
             anim.add_window(handle, wid, current_frame, target_frame, is_new);
         }
         anim.run();
+        //anim.skip_to_end();
     }
 }
 
-pub fn calculate_layout(screen: CGRect, windows: &Vec<(&AppInfo, &WindowInfo)>) -> Vec<CGRect> {
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+enum Layout {
+    Slice(Orientation),
+    Bsp(Orientation),
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Orientation {
+    Horizontal,
+    Vertical,
+}
+
+fn calculate_layout(
+    screen: CGRect,
+    windows: &[(&AppInfo, &WindowInfo)],
+    layout: Layout,
+) -> Vec<CGRect> {
+    use Layout::*;
+    use Orientation::*;
     let num_windows: u32 = windows.len().try_into().unwrap();
-    let width = screen.size.width / f64::from(num_windows);
-    // TODO: Convert between coordinate systems.
-    (0..num_windows)
-        .map(|i| CGRect {
-            origin: CGPoint {
-                x: screen.origin.x + f64::from(i) * width,
-                y: screen.origin.y,
-            },
-            size: CGSize {
-                width,
+    if num_windows == 0 {
+        return vec![];
+    }
+    if num_windows == 1 {
+        return vec![screen];
+    }
+    match layout {
+        Slice(Horizontal) => {
+            let width = screen.size.width / f64::from(num_windows);
+            (0..num_windows)
+                .map(|i| CGRect {
+                    origin: CGPoint {
+                        x: screen.origin.x + f64::from(i) * width,
+                        y: screen.origin.y,
+                    },
+                    size: CGSize {
+                        width,
+                        height: screen.size.height,
+                    },
+                })
+                .collect()
+        }
+        Slice(Vertical) => todo!(),
+        Bsp(Horizontal) => {
+            let size = CGSize {
+                width: screen.size.width / 2.0,
                 height: screen.size.height,
-            },
-        })
-        .collect()
+            };
+
+            let window_frame = CGRect { origin: screen.origin, size };
+            let remainder = CGRect {
+                origin: CGPoint {
+                    x: screen.origin.x + size.width,
+                    y: screen.origin.y,
+                },
+                size,
+            };
+
+            iter::once(window_frame)
+                .chain(calculate_layout(
+                    remainder,
+                    &windows[1..],
+                    Layout::Bsp(Orientation::Vertical),
+                ))
+                .collect()
+        }
+        Layout::Bsp(Orientation::Vertical) => {
+            let size = CGSize {
+                width: screen.size.width,
+                height: screen.size.height / 2.0,
+            };
+
+            let window_frame = CGRect { origin: screen.origin, size };
+            let remainder = CGRect {
+                origin: CGPoint {
+                    x: screen.origin.x,
+                    y: screen.origin.y + size.height,
+                },
+                size,
+            };
+
+            iter::once(window_frame)
+                .chain(calculate_layout(
+                    remainder,
+                    &windows[1..],
+                    Layout::Bsp(Orientation::Horizontal),
+                ))
+                .collect()
+        }
+    }
 }
